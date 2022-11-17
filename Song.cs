99372c90b511ab80d547b68b1fdd380dcf5770eb
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-11-16 20:48:02 -0500

    Add files via upload

---
+++ Song.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicAPI.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Author Author { get; set; }
+    }
+}
