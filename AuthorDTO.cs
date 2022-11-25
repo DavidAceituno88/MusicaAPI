0d09018fae924de21f6f07c24a1b2db26db71c5b
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-11-25 18:55:52 -0500

    Update AuthorDTO.cs

--- AuthorDTO.cs
+++ AuthorDTO.cs
@@ -3,6 +3,6 @@ namespace MusicAPI.Models
     public class AuthorDTO
     {
         public string Name { get; set; }
-        public List<Song>? Songs { get; set; }
+        public List<SongOnlyDTO>? Songs { get; set; }
     }
 }
