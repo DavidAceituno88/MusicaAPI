112f1fdb0a54a9783f20b01c56b982f18b334f11
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-11-24 20:53:34 -0500

    Create songDTO.cs

---
+++ songDTO.cs
@@ -0,0 +1,10 @@
+namespace MusicAPI.Models
+{
+    public class SongDTO
+    {
+        public string Name { get; set; }
+        public int AuthorId { get; set; }
+
+        public string AuthorName { get; set; }  
+    }
+}
